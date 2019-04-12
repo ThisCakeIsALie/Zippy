1dcad90044ed2383ab35554db44087efc6984af2
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2019-04-12 17:58:05 +0200

    Slightly updated compression

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import * as blessed from "blessed";
 import * as del from "del";
 import { ArchiveList } from "./ArchiveList";
 import { pathToFlatArchive, toArchiveTree, ArchiveTree } from "../ZipHandler";
+import { relative } from 'path';
 
 interface AppState {
   archive: ArchiveTree;
@@ -34,8 +35,6 @@ export class App extends React.Component {
     });
   }
 
-  private calcArchiveTree = toArchiveTree;
-
   render() {
     const archiveTree = this.state.archive;
 
@@ -48,18 +47,27 @@ export class App extends React.Component {
 
           }}
           onSelect={async selected => {
+            const archive = pathToFlatArchive(this.props.path);
+
             if (selected.isOpen) {
-              if (selected.entry === "root") {
-                await del(this.props.path, { force: true });
-              } else {
-                await del("./" + selected.entry.entryName);
+              const delPath = selected.entry === "root" ? this.props.path + "/**" : "./" + selected.entry.entryName;
+              const pathsToDelete = await del(delPath, { dryRun: true });
+              for (let path of pathsToDelete) {
+                const relPath = relative(".", path).replace(/\\/g, "/");
+                const entry = archive.getEntry(relPath);
+                if (entry != null) {
+                  if (!entry.isDirectory) {
+                    await archive.updateFile(relPath, require("fs").readFileSync(relPath));
+                  }
+                }
               }
+              await archive.writeZip(this.props.path + ".zip");
+              await del(pathsToDelete);
               this.setState({ archive: toArchiveTree(this.props.path) });
               return;
             }
 
             const selectedEntry = selected.entry;
-            const archive = pathToFlatArchive(this.props.path);
 
             if (selectedEntry === "root") {
               await archive.extractAllTo(".", true);
