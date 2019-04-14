721a449c61ce1e9f6c53b99bbaa177cc1469fdfc
ThisCakeIsALie <37264026+ThisCakeIsALie@users.noreply.github.com>
2019-04-14 22:00:57 +0200

    Finished Archiver

--- src/Archiver.ts
+++ src/Archiver.ts
@@ -1,6 +1,8 @@
 import * as Zip from "adm-zip";
 import { paths } from "node-dir";
 import { basename, relative } from "path";
+import { lstatSync, existsSync, mkdirSync } from "fs";
+import * as del from "del";
 
 type EntryStatus = "compressed" | "uncompressed";
 type EntryInfo = undefined;
@@ -10,48 +12,122 @@ interface Entry {
   path: string;
   status: EntryStatus;
   info: EntryInfo;
+  isDirectory: boolean;
   archiveEntry: Zip.IZipEntry | null;
 }
 
+type EntryLike = Entry | string;
+
 export class Archiver {
+  private readonly zipPath: string;
   private archive: Zip;
   private entries: Entry[];
 
-  constructor(private path: string) {
-    this.refresh();
+  private constructor(private readonly path: string) {
+    this.zipPath = relative(".", path) + ".zip";
+  }
+
+  static async forPath(path): Promise<Archiver> {
+    const archiver = new Archiver(path);
+    await archiver.refresh();
+    return archiver;
   }
 
   async refresh(): Promise<void> {
-    this.archive = new Zip(this.path);
+    if (!existsSync(this.path)) {
+      mkdirSync(this.path);
+    }
+    if (!existsSync(this.zipPath)) {
+      await new Zip().writeZip(this.zipPath);
+    }
+    this.archive = await new Zip(this.zipPath);
     this.entries = await this.initializeEntries();
   }
 
-  compress(entryPath: string): void {}
+  async compress(entry: EntryLike): Promise<void> {
+    const actualEntry = this.getEntry(entry);
+    if (actualEntry.status === "compressed") {
+      throw new Error("Entry is already compressed");
+    }
+    if (actualEntry.isDirectory) {
+      //For some reasom addLocalFolder doesn't add this by itself, so we add the directory entry manually
+      await this.archive.addFile(actualEntry.path, Buffer.alloc(0));
+      await this.archive.addLocalFolder(actualEntry.path, actualEntry.path);
+    } else {
+      await this.archive.addLocalFile(actualEntry.path, actualEntry.path);
+    }
+    await this.archive.writeZip(this.zipPath);
+    await del(actualEntry.path);
+    await this.refresh();
+  }
 
-  decompress(entryPath: string): void {}
+  async decompress(entry: EntryLike): Promise<void> {
+    const actualEntry = this.getEntry(entry);
+    if (actualEntry.status === "uncompressed") {
+      throw new Error("Entry is already decompressed");
+    }
+    await this.archive.extractEntryTo(
+      actualEntry.archiveEntry,
+      actualEntry.path,
+      false,
+      true
+    );
+    await this.archive.deleteFile(actualEntry.archiveEntry);
+    await this.archive.writeZip(this.zipPath);
+    await this.refresh();
+  }
 
   getEntries(type: EntryStatus | "all" = "all") {
-      switch(type) {
-          case "compressed":
-            return this.entries.filter(entry => entry.status === "compressed");
-          case "uncompressed":
-            return this.entries.filter(entry => entry.status === "uncompressed");
-          case "all":
-            return this.entries;
-      }
+    switch (type) {
+      case "compressed":
+        return this.entries.filter(entry => entry.status === "compressed");
+      case "uncompressed":
+        return this.entries.filter(entry => entry.status === "uncompressed");
+      case "all":
+        return this.entries;
+    }
   }
 
-  getEntry(entry: Entry | string) {
-      if (typeof entry !== "string") {
-          return entry;
-      }
-
-      return this.entries.find(val => val.path === entry || val.name === entry);
+  getEntry(entry: EntryLike): Entry {
+    if (typeof entry !== "string") {
+      return entry;
+    }
+    return this.entries.find(val => val.path === entry || val.name === entry);
   }
 
-  childrenOf(entryPath: string) {}
+  childrenOf(entry: EntryLike | "root"): Entry[] {
+    let actualEntry: Entry = null;
 
-  parentOf(entryPath: string) {}
+    if (typeof entry === "string") {
+      if (entry === "root") {
+        return this.entries.filter(otherEntry => this.getPathParts(otherEntry).length === 2);
+      }
+      actualEntry = this.getEntry(entry);
+    } else {
+      actualEntry = entry;
+    }
+    
+    const path = this.getPathParts(actualEntry);
+
+    return this.entries.filter(otherEntry => {
+      const otherPath = this.getPathParts(otherEntry);
+      if (path.length + 1 !== otherPath.length) {
+        return false;
+      }
+      return path.every((part, i) => part === otherPath[i]);
+    });
+  }
+
+  parentOf(entry: EntryLike): Entry {
+    const actualEntry =
+      typeof entry !== "string" ? entry : this.getEntry(entry);
+    return this.getEntry(this.getPathParts(actualEntry).join("/") + "/");
+  }
+
+  private getPathParts(entry: Entry) {
+    const splitPath = entry.path.replace(/^[/]+|[/]+$/g, "").split("/");
+    return splitPath;
+  }
 
   private async initializeEntries(): Promise<Entry[]> {
     //Initializes all compressed entries
@@ -59,8 +135,9 @@ export class Archiver {
       return {
         name: archiveEntry.name,
         path: archiveEntry.entryName,
-        status: "compressed",
+        status: "compressed" as EntryStatus,
         info: undefined,
+        isDirectory: archiveEntry.isDirectory,
         archiveEntry
       };
     });
@@ -68,8 +145,8 @@ export class Archiver {
     const uncompressed: Entry[] = await new Promise((resolve, reject) => {
       paths(this.path, true, (err, subpaths) => {
         if (err !== null) {
-            reject(err);
-            return;
+          reject(err);
+          return;
         }
         if (!Array.isArray(subpaths)) {
           reject(new Error("Unexpected subpath result while decompressing"));
@@ -78,11 +155,15 @@ export class Archiver {
 
         resolve(
           subpaths.map(path => {
+            const isDirectory = lstatSync(path).isDirectory();
             return {
               name: basename(path),
-              path: relative(".", path).replace(/\\/g, "/"),
-              status: "uncompressed",
+              path:
+                relative(".", path).replace(/\\/g, "/") +
+                (isDirectory ? "/" : ""),
+              status: "uncompressed" as EntryStatus,
               info: undefined,
+              isDirectory,
               archiveEntry: null
             };
           })
@@ -92,5 +173,4 @@ export class Archiver {
 
     return [...compressed, ...uncompressed];
   }
-
 }
